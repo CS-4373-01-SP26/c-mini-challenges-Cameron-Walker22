0d2d0e24836b63d666a1ce407f3522bb2b938216
Cameron-Walker22 <cameronwalker779@gmail.com>
2026-02-15 08:19:04 -0600

    Finished Problem 2

---
+++ archimedes.c
@@ -0,0 +1,41 @@
+/* 
+    Cameron Walker
+    Dr. John Hale
+    CS-4373 High Performance Computing
+    2/11/26
+
+    C Mini Challenges: Problem 2
+    Implement Archimedes algorithm to estimate pi for inscribed/circumscribed polygons 
+    with n sides, up to 100, doubling n at each step, and time it.
+*/
+
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#define M_PI 3.14159265358979323846
+
+int main(void) {
+
+    int max_n = 100; // Stopping Criteria
+    int start_n = 3; // Starting Criteria
+
+    int n = start_n; // Looping Variable (Sides of Polygons)
+    while(n <= max_n) {
+        clock_t start = clock();
+
+        double theta = M_PI/n;
+
+        double pi_lower = n*sin(theta); // Inscribed Polygon
+        double pi_upper = n*tan(theta); // Circumscribed Polygon
+
+        clock_t end = clock();
+        double elapsed = ((double) (end-start)) / CLOCKS_PER_SEC; // Elapsed Time
+
+        printf("n = %d, pi_lower = %f, pi_upper = %f, diff = %f, time = %f s\n",
+                n, pi_lower, pi_upper, pi_upper - pi_lower, elapsed);
+
+        n *= 2;
+    }
+
+    return 0;
+}
